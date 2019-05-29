5ff9db84720c1b898015be3684c76bcc0d2ab710
Ronnie Krupnik <ronnie.krup@gmail.com>
2019-05-29 13:41:57 +0300

    code writen, 4 out of 12 tests passed

--- hw5.py
+++ hw5.py
@@ -42,9 +42,40 @@ class QuestionnaireAnalysis:
         Returns the corrected DataFrame, i.e. the same table but with
         the erroneous rows removed and the (ordinal) index after a reset.
         """
+        email_reg = '.+@.+(.).+'
+        self.data = self.data[self.data['email'].str.contains(email_reg)]
+        truth = pd.read_csv(r'tests_data\q2_email.csv')
+        [print(i, j) for i, j in zip(self.data['email'], truth['email']) if not i == j]
+        return self.data
 
+    def fill_na_with_mean(self) -> Union[pd.DataFrame, np.ndarray]:
+        """
+        Finds, in the original DataFrame, the subjects that didn't answer
+        all questions, and replaces that missing value with the mean of the
+        other grades for that student. Returns the corrected DataFrame,
+        as well as the row indices of the students that their new grades
+        were generated.
+        """
+        inds = np.array([])
+        questions = ['q1', 'q2', 'q3', 'q4', 'q5']
+        for q in questions:
+            nans = self.data[q].isna()
+            self.data.loc[nans, q] = np.nanmean(self.data[questions][nans], 1)
+            inds = np.union1d(inds, nans[nans].index)
+        return [self.data, inds]
+
+    def correlate_gender_age(self) -> pd.DataFrame:
+        """
+        Looks for a correlation between the gender of the subject, their age
+        and the score for all five questions.
+        Returns a DataFrame with a MultiIndex containing the gender and whether
+        the subject is above 40 years of age, and the average score in each of
+        the five questions.
+        """
+        g = self.data[['gender', 'q1', 'q1', 'q1', 'q1']].groupby(['gender', self.data.age > 40])
+        return g.mean()
 
 
 if __name__ == '__main__':
     qa = QuestionnaireAnalysis('data.json')
-    print(qa.show_age_distrib())
+    qa.remove_rows_without_mail()
